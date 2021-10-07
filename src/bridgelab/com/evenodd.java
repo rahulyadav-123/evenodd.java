ec2a82c7d908473973423bd30caf0b1daae35a54
rahul yadav <ry219613@gmail.com>
2021-10-08 02:09:15 +0530

    print number even or odd

---
+++ src/bridgelab/com/evenodd.java
@@ -0,0 +1,22 @@
+package bridgelab.com;
+
+import java.util.Scanner;
+
+public class evenodd {
+	public static void main(String[] args) {
+		
+	
+	Scanner scanner = new Scanner(System.in);
+	System.out.print("Enter a number: ");
+	int number=scanner.nextInt();
+	if(number % 2 == 0) {
+		System.err.println("Given number is even");
+	}
+	else {
+		System.out.println("Given number is odd");
+	}
+}
+
+}
+
+
